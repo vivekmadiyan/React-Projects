b852d882b847f4e33e093df957526ab1da555a7b
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-11-19 23:45:03 +0530

    Update StartGame.jsx

--- Dice-Game/src/Components/StartGame.jsx
+++ Dice-Game/src/Components/StartGame.jsx
@@ -1,11 +1,44 @@
+
 import React from 'react'
+import styled from 'styled-components';
+
 
 function StartGame() {
   return (
+    <Container>
     <div>
+      <img 
+      src='/Images/dices.png'/>
+      </div>
       
-    </div>
+      <div className='content'>
+        <h1>
+            DICE GAME
+        </h1>
+        <Button>Play Now</Button>
+      </div>
+    </Container>
   )
 }
 
-export default StartGame
+export default StartGame;
+const Container=styled.div`
+max-width:1180px;
+display: flex;
+margin: 0 auto;
+height: 100vh;
+align-items: center;
+.content{
+    h1{
+        font-size: 98px;
+        white-space: nowrap;
+    }
+}
+`;
+const Button=styled.button`
+color: white;
+padding: 10px 18px;
+background: #000000;
+border-radius: 5px;
+border: none;
+`;
