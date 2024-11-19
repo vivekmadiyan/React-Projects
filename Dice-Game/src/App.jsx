9bfbf40289397d27b63455a775c6ca30fc2f8e6b
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-11-19 23:27:38 +0530

    Update App.jsx

--- Dice-Game/src/App.jsx
+++ Dice-Game/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import styled from "styled-components";
+
+const Button=styled.button`
+background-color:black;
+color:white;
+padding:10px;
+`;
 
 function App() {
   return (
     <div>
-   
+   <Button>Styled Component</Button>
     </div>
   )
 }
