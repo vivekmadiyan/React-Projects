10e257d391493d26bb87bd7400ef308301cf08cb
vivekmadiyan <vivekmadiyan3536@gmail.com>
2024-11-29 01:58:41 +0530

    Update NumberSelector.jsx

--- Dice-Game/src/Components/NumberSelector.jsx
+++ Dice-Game/src/Components/NumberSelector.jsx
@@ -1,41 +1,64 @@
-import React, { useState } from 'react'
-import styled from "styled-components"
+import styled from "styled-components";
+const NumberSelector = ({
+  setError,
+  error,
+  selectedNumber,
+  setSelectedNumber,
+}) => {
+  const arrNumber = [1, 2, 3, 4, 5, 6];
 
-function NumberSelector() {
-    const[selectedNumber , setselectedNumber]=useState();
-    const arrNumber =[1,2,3,4,5,6];
-  
+  const numberSelectorHandler = (value) => {
+    setSelectedNumber(value);
+    setError("");
+  };
 
   return (
     <NumberSelectorContainer>
-        <div className='flex'>
-        {
-            arrNumber.map((value,i)=>(<Box isSelected={value==selectedNumber}
-            key={i} onClick={()=>setselectedNumber(value)}
-            >{value}</Box>))
-        }
-        </div>
-        <p>Select Number</p>
-  
+      <p className="error">{error}</p>
+      <div className="flex">
+        {arrNumber.map((value, i) => (
+          <Box
+            isSelected={value === selectedNumber}
+            key={i}
+            onClick={() => numberSelectorHandler(value)}
+          >
+            {value}
+          </Box>
+        ))}
+      </div>
+      <p>Select Number</p>
     </NumberSelectorContainer>
-  )
-}
+  );
+};
 
-export default NumberSelector
-const NumberSelectorContainer=styled.div`
-.flex{
- display: flex;
- gap: 24px;
-}
- `;
- const Box=styled.div `
-    height: 72px;
-    width: 72px;
-    border: 1px solid black;
-    display: grid;
-    place-items: center;
+export default NumberSelector;
+
+const NumberSelectorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+
+  .flex {
+    display: flex;
+    gap: 24px;
+  }
+  p {
     font-size: 24px;
-    font-weight: 700;
-    background-color: ${(props)=>props.isSelected ? "black ": "white"};
-    color: ${(props)=>props.isSelected ? "white ": "black"};
- `;
+    font-weight: 700px;
+  }
+  .error {
+    color: red;
+  }
+`;
+
+const Box = styled.div`
+  height: 72px;
+  width: 72px;
+  border: 1px solid black;
+  display: grid;
+  place-items: center;
+  font-size: 24px;
+  font-weight: 700;
+  background-color: ${(props) => (props.isSelected ? "black" : "white")};
+  color: ${(props) => (!props.isSelected ? "black" : "white")};
+`;
